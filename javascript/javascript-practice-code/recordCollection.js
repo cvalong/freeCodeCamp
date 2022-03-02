ea65995b31f50bece8803adf06f15b5454fcf5c0
cvalong <chris.va.long@gmail.com>
2022-03-02 14:43:56 -0800

    1 file modified

--- javascript/javascript-practice-code/recordCollection.js
+++ javascript/javascript-practice-code/recordCollection.js
@@ -1,42 +1,48 @@
+/*You start with an updateRecords function that takes an object literal, records, containing the musical album collection, an id, a prop (like artist or tracks), and a value. Complete the function using the rules below to modify the object passed to the function.
+
+Your function must always return the entire record collection object.
+If prop isn't tracks and value isn't an empty string, update or set that album's prop to value.
+If prop is tracks but the album doesn't have a tracks property, create an empty array and add value to it.
+If prop is tracks and value isn't an empty string, add value to the end of the album's existing tracks array.
+If value is an empty string, delete the given prop property from the album.
+*/
 // Setup
 const recordCollection = {
   2548: {
     albumTitle: 'Slippery When Wet',
     artist: 'Bon Jovi',
-    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+    tracks: ['Let It Rock', 'You Give Love a Bad Name'],
   },
   2468: {
     albumTitle: '1999',
     artist: 'Prince',
-    tracks: ['1999', 'Little Red Corvette']
+    tracks: ['1999', 'Little Red Corvette'],
   },
   1245: {
     artist: 'Robert Palmer',
-    tracks: []
+    tracks: [],
   },
   5439: {
-    albumTitle: 'ABBA Gold'
-  }
+    albumTitle: 'ABBA Gold',
+  },
 };
 
 // Only change code below this line
 function updateRecords(records, id, prop, value) {
-  let propUpdate = "tracks";
-  if (value == "") {
+  let propUpdate = 'tracks';
+  if (value === '') {
     delete records[id][prop];
-  } else if (prop != propUpdate) {
-    records[id][prop] = value;
-  } else {
-    if (!(records.hasOwnProperty(propUpdate))) {
+  } else if (prop === propUpdate) {
+    if (!records[id].hasOwnProperty(propUpdate)) {
       records[id][propUpdate] = [value];
-    }
-    else {
+    } else {
       records[id][propUpdate].push(value);
-      console.log(records[id][propUpdate]);
     }
+  } else {
+    records[id][prop] = value;
   }
   return records;
 }
 
 //updateRecords(recordCollection, 5439, 'artist', 'ABBA');
-updateRecords(recordCollection, 2468, "tracks", "Free");
+updateRecords(recordCollection, 2468, 'tracks', 'Free');
