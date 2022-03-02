ba8fb209138c6b88c3c8d0257f471e46aa8a1823
cvalong <chris.va.long@gmail.com>
2022-03-02 14:22:09 -0800

    1 file modified

--- javascript/javascript-practice-code/recordCollection.js
+++ javascript/javascript-practice-code/recordCollection.js
@@ -1,15 +1,3 @@
-/*You are given an object literal representing a part of your musical album collection. Each album has a unique id number as its key and several other properties. Not all albums have complete information.
-
-You start with an updateRecords function that takes an object literal, records, containing the musical album collection, an id, a prop (like artist or tracks), and a value. Complete the function using the rules below to modify the object passed to the function.
-
-Your function must always return the entire record collection object.
-If prop isn't tracks and value isn't an empty string, update or set that album's prop to value.
-If prop is tracks but the album doesn't have a tracks property, create an empty array and add value to it.
-If prop is tracks and value isn't an empty string, add value to the end of the album's existing tracks array.
-If value is an empty string, delete the given prop property from the album.
-Note: A copy of the recordCollection object is used for the tests.
-*/
-
 // Setup
 const recordCollection = {
   2548: {
@@ -33,7 +21,22 @@ const recordCollection = {
 
 // Only change code below this line
 function updateRecords(records, id, prop, value) {
+  let propUpdate = "tracks";
+  if (value == "") {
+    delete records[id][prop];
+  } else if (prop != propUpdate) {
+    records[id][prop] = value;
+  } else {
+    if (!(records.hasOwnProperty(propUpdate))) {
+      records[id][propUpdate] = [value];
+    }
+    else {
+      records[id][propUpdate].push(value);
+      console.log(records[id][propUpdate]);
+    }
+  }
   return records;
 }
 
-updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+//updateRecords(recordCollection, 5439, 'artist', 'ABBA');
+updateRecords(recordCollection, 2468, "tracks", "Free");
